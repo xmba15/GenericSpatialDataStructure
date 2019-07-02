3112b4a734f1768916c8d0cdef0d5c9f4827461c
xmba15 <thba1590@gmail.com>
2019-07-03 01:44:34 +0900

    small fix for examples

--- examples/ExOctree.cpp
+++ examples/ExOctree.cpp
@@ -11,66 +11,109 @@
  *
  */
 
+#include <chrono>
+#include <iomanip>
 #include <iostream>
+#include <random>
 #include <spatial_data_structure/Octree.hpp>
 
 int main(int argc, char *argv[])
 {
     using Octree = algo::Octree<double>;
-    Octree::BoundingBox3D bbox(0, 0, 0, 100, 100, 100);
-    Octree::Ptr octree = std::make_shared<Octree>(bbox);
-    Octree::VecPointType pV{{30, 57, 15}, {15, 6, 78},   {68, 70, 43}, {-1, 5, -4},
-                            {10, 67, 30}, {45, 9, 16.7}, {100, 6, 0},  {20, 45, 65.3}};
 
-    octree->insertElems(pV);
+    {
+        Octree::BoundingBox3D bbox(0, 0, 0, 100, 100, 100);
+        Octree::Ptr octree = std::make_shared<Octree>(bbox);
+        Octree::VecPointType pV{{30, 57, 15}, {15, 6, 78},    {68, 70, 43}, {-1, 5, -4},  {10, 67, 30}, {45, 9, 16.7},
+                                {100, 6, 0},  {20, 45, 65.3}, {45, 67, 89}, {12, 34, 56}, {67, 12, 34}};
 
-    std::cout << "Number of points: " << octree->pointSize() << "\n";
+        octree->insertElems(pV);
 
-    Octree::VecPointType v = octree->queryRange(bbox);
+        Octree::VecPointType v = octree->queryRange(bbox);
 
-    for (auto &elem : v) {
-        std::cout << elem << "\n";
-        std::cout << "---"
-                  << "\n";
+        Octree::PointType pointToCheck{45, 9, 18};
+
+        Octree::Ptr octantPtr = octree->searchElem(Octree::PointType{20, 45, 65.3});
+
+        if (octantPtr) {
+            std::cout << "found the element \n";
+            std::cout << octantPtr->bbox() << "\n";
+            std::cout << octantPtr->points()[0] << "\n";
+        } else {
+            std::cout << "not found \n";
+        }
+
+        Octree::VecPointType knn = octree->nearestSearch(Octree::PointType{45, 9, 18}, 4);
+
+        octree->deleteElems(pV);
     }
 
-    Octree::PointType pointToCheck{45, 9, 18};
-    std::cout << "distance: \n";
-    for (auto &elem : octree->queryRange(bbox)) {
-        std::cout << "-------------\n";
-        std::cout << elem << "\n";
-        std::cout << "distance: " << (elem - pointToCheck).norm() << "\n";
-    }
-    std::cout << "end of calculating distance: \n";
+    {
+        std::cout << "test computation time between naive nearest search && knn by octree: \n";
+        const double LOWER_BOUND = 0;
+        const double UPPER_BOUND = 200;
+        const int NUM_POINTS = 50;
+        const int NUM_POINTS_TO_CHECK = 10;
+        const int K = 4;
 
-    Octree::Ptr octantPtr = octree->searchElem(Octree::PointType{20, 45, 65.3});
+        Octree::VecPointType points;
+        Octree::VecPointType pointsToCheck;
+        points.reserve(NUM_POINTS);
+        pointsToCheck.reserve(NUM_POINTS_TO_CHECK);
 
-    if (octantPtr) {
-        std::cout << "found the element"
-                  << "\n";
-        std::cout << octantPtr->bbox() << "\n";
-        std::cout << octantPtr->points()[0] << "\n";
-    } else {
-        std::cout << "nothing found"
-                  << "\n";
-    }
+        std::uniform_real_distribution<double> unif(LOWER_BOUND, UPPER_BOUND);
+        std::default_random_engine re;
 
-    Octree::VecPointType knn = octree->nearestSearch(Octree::PointType{45, 9, 18}, 4);
+        for (size_t i = 0; i < NUM_POINTS; ++i) {
+            double x = unif(re);
+            double y = unif(re);
+            double z = unif(re);
+            points.emplace_back(Octree::PointType{x, y, z});
+        }
 
-    std::cout << "knn result"
-              << "\n";
-    for (auto &elem : knn) {
-        std::cout << elem << "\n";
-        std::cout << "---"
-                  << "\n";
-    }
+        for (size_t i = 0; i < NUM_POINTS_TO_CHECK; ++i) {
+            double x = unif(re);
+            double y = unif(re);
+            double z = unif(re);
+            pointsToCheck.emplace_back(Octree::PointType{x, y, z});
+        }
 
-    octree->deleteElems(pV);
-    std::cout << "after deleting\n";
-    for (auto &elem : octree->points()) {
-        std::cout << elem << "\n";
-        std::cout << "---"
-                  << "\n";
+        {
+            std::cout << "naive method: \n";
+            auto start = std::chrono::high_resolution_clock::now();
+            for (const Octree::PointType &pointToCheck : pointsToCheck) {
+                auto tempPV(points);
+                std::sort(tempPV.begin(), tempPV.end(),
+                          [pointToCheck](const Octree::PointType &p1, const Octree::PointType &p2) {
+                              return (p1 - pointToCheck).norm() < (p2 - pointToCheck).norm();
+                          });
+            }
+
+            auto end = std::chrono::high_resolution_clock::now();
+            double executedTime = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+
+            executedTime *= 1e-9;
+            std::cout << "Time taken by program is : " << std::fixed << executedTime << std::setprecision(6);
+            std::cout << " sec\n";
+        }
+
+        {
+            std::cout << "knn by octree: \n";
+            auto start = std::chrono::high_resolution_clock::now();
+            Octree::BoundingBox3D bbox(LOWER_BOUND, LOWER_BOUND, LOWER_BOUND, UPPER_BOUND, UPPER_BOUND, UPPER_BOUND);
+            Octree::Ptr octree = std::make_shared<Octree>(bbox);
+            octree->insertElems(points);
+
+            for (const Octree::PointType &pointToCheck : pointsToCheck) {
+                Octree::VecPointType knn = octree->nearestSearch(pointToCheck, K);
+            }
+
+            auto end = std::chrono::high_resolution_clock::now();
+            double executedTime = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+            executedTime *= 1e-9;
+            std::cout << "Time taken by program is : " << std::fixed << executedTime << std::setprecision(6);
+            std::cout << " sec\n";
+        }
     }
 
     return 0;
--- examples/ExQuadtree.cpp
+++ examples/ExQuadtree.cpp
@@ -11,56 +11,103 @@
  *
  */
 
+#include <chrono>
+#include <iomanip>
 #include <iostream>
+#include <random>
 #include <spatial_data_structure/Quadtree.hpp>
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     using Quadtree = algo::Quadtree<double>;
-    Quadtree::BoundingBox bbox(0, 0, 100, 100);
+    {
+        Quadtree::BoundingBox bbox(0, 0, 100, 100);
 
-    Quadtree::Ptr quadTree = std::make_shared<Quadtree>(bbox);
-    Quadtree::VecPointType pV{{30, 57}, {15, 6}, {68, 70}, {-1, 5}, {10, 67}, {45, 9}, {100, 6}, {20, 45}};
+        Quadtree::Ptr quadtree = std::make_shared<Quadtree>(bbox);
+        Quadtree::VecPointType points{{30, 57}, {15, 6}, {68, 70}, {-1, 5}, {10, 67}, {45, 9}, {100, 6}, {20, 45}};
 
-    quadTree->insertElems(pV);
+        quadtree->insertElems(points);
 
-    Quadtree::PointType pointToCheck{10, 20};
-    std::cout << "distance: \n";
-    for (auto& elem : quadTree->queryRange(bbox)) {
-        std::cout << "-------------\n";
-        std::cout << elem << "\n";
-        std::cout << "distance: " << (elem - pointToCheck).norm() << "\n";
-    }
-    std::cout << "end of calculating distance: \n";
+        Quadtree::VecPointType knn = quadtree->nearestSearch(Quadtree::PointType{10, 20}, 4);
 
-    Quadtree::VecPointType knn = quadTree->nearestSearch(Quadtree::PointType{10, 20}, 4);
-    std::cout << "k nearest search\n";
-    for (auto& elem : knn) {
-        std::cout << elem << "\n";
-        std::cout << "---\n";
-    }
+        quadtree->deleteElems(points);
 
-    std::cout << "end of k nearest search \n";
+        Quadtree::VecPointType v = quadtree->queryRange(bbox);
 
-    quadTree->deleteElems(pV);
+        Quadtree::Ptr quadrantPtr = quadtree->searchElem(Quadtree::PointType{15, 6});
 
-    Quadtree::VecPointType v = quadTree->queryRange(bbox);
-
-    for (auto& elem : v) {
-        std::cout << elem << "\n";
-        std::cout << "---"
-                  << "\n";
+        if (quadrantPtr) {
+            std::cout << "found the element in\n";
+            std::cout << quadrantPtr->bbox() << "\n";
+        } else {
+            std::cout << "not found \n";
+        }
     }
 
-    Quadtree::Ptr quadrantPtr = quadTree->searchElem(Quadtree::PointType{15, 6});
+    {
+        std::cout << "test computation time between naive nearest search && knn by quadtree: \n";
+        const double LOWER_BOUND = -500;
+        const double UPPER_BOUND = 500;
+        const int NUM_POINTS = 400;
+        const int NUM_POINTS_TO_CHECK = 10;
+        const int K = 10;
 
-    if (quadrantPtr) {
-        std::cout << "found the element"
-                  << "\n";
-        std::cout << quadrantPtr->bbox() << "\n";
-    } else {
-        std::cout << "nothing found"
-                  << "\n";
+        Quadtree::VecPointType points;
+        Quadtree::VecPointType pointsToCheck;
+        points.reserve(NUM_POINTS);
+        pointsToCheck.reserve(NUM_POINTS_TO_CHECK);
+
+        std::uniform_real_distribution<double> unif(LOWER_BOUND, UPPER_BOUND);
+        std::default_random_engine re;
+
+        for (size_t i = 0; i < NUM_POINTS; ++i) {
+            double x = unif(re);
+            double y = unif(re);
+            points.emplace_back(Quadtree::PointType{x, y});
+        }
+
+        for (size_t i = 0; i < NUM_POINTS_TO_CHECK; ++i) {
+            double x = unif(re);
+            double y = unif(re);
+            pointsToCheck.emplace_back(Quadtree::PointType{x, y});
+        }
+
+        {
+            std::cout << "naive method: \n";
+            auto start = std::chrono::high_resolution_clock::now();
+            for (const Quadtree::PointType &pointToCheck : pointsToCheck) {
+                auto tempPV(points);
+                std::sort(tempPV.begin(), tempPV.end(),
+                          [pointToCheck](const Quadtree::PointType &p1, const Quadtree::PointType &p2) {
+                              return (p1 - pointToCheck).norm() < (p2 - pointToCheck).norm();
+                          });
+            }
+
+            auto end = std::chrono::high_resolution_clock::now();
+            double executedTime = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+
+            executedTime *= 1e-9;
+            std::cout << "Time taken by program is : " << std::fixed << executedTime << std::setprecision(6);
+            std::cout << " sec\n";
+        }
+
+        {
+            std::cout << "knn by quadtree: \n";
+            auto start = std::chrono::high_resolution_clock::now();
+            Quadtree::BoundingBox bbox(LOWER_BOUND, LOWER_BOUND, UPPER_BOUND, UPPER_BOUND);
+            Quadtree::Ptr quadtree = std::make_shared<Quadtree>(bbox);
+            quadtree->insertElems(points);
+
+            for (const Quadtree::PointType &pointToCheck : pointsToCheck) {
+                Quadtree::VecPointType knn = quadtree->nearestSearch(pointToCheck, K);
+            }
+
+            auto end = std::chrono::high_resolution_clock::now();
+            double executedTime = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+            executedTime *= 1e-9;
+            std::cout << "Time taken by program is : " << std::fixed << executedTime << std::setprecision(6);
+            std::cout << " sec\n";
+        }
     }
 
     return 0;
